f3b5eac0b2bd6aee8179cf672e1c8c633b4437c7
tumakha <tumakha@gmail.com>
2011-12-23 11:16:33 +0000

    create web project

---
+++ src/main/java/ua/org/tumakha/util/NumberUtil.java
@@ -0,0 +1,16 @@
+package ua.org.tumakha.util;
+
+/**
+ * @author Yuriy Tumakha
+ */
+public class NumberUtil {
+
+	public static String numberInWordsEn(long number) {
+		return NumberUtilEn.numberInWords(number);
+	}
+
+	public static String numberInWordsUa(long number) {
+		return NumberUtilUa.numberInWords(number);
+	}
+
+}
