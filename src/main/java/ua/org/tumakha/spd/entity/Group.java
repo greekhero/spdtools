8d86f85775b24bd222221e544eed44df8081e294
tumakha <tumakha@gmail.com>
2011-12-27 09:28:39 +0000

    entity Group

---
+++ src/main/java/ua/org/tumakha/spd/entity/Group.java
@@ -0,0 +1,54 @@
+package ua.org.tumakha.spd.entity;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+/**
+ * @author Yuriy Tumakha
+ */
+@Entity
+@Table(name = "user_group")
+public class Group {
+
+    @Id
+    @GeneratedValue
+    private Integer groupId;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "groups")
+    @JoinTable(name = "user_group_mapping", joinColumns = @JoinColumn(name = "groupId", referencedColumnName = "groupId"), inverseJoinColumns = @JoinColumn(name = "userId", referencedColumnName = "userId"))
+    private Set<User> users;
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+}
