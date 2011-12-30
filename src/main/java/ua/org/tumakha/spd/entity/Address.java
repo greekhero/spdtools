66ceaabcace9831db455ddecd8971b127c8479b1
tumakha <tumakha@gmail.com>
2011-12-30 17:43:17 +0000

    fix street and district print

--- src/main/java/ua/org/tumakha/spd/entity/Address.java
+++ src/main/java/ua/org/tumakha/spd/entity/Address.java
@@ -19,245 +19,260 @@ import ua.org.tumakha.util.StrUtil;
 @Table(name = "address")
 public class Address {
 
-    private static final String DELIMITER = ", ";
-    private static final String REGION_SUFIX = " oбл.";
-    private static final String REGION_SUFIX_EN = " region";
-    private static final String STREET_PREFIX = "вул. ";
-    // TODO: use sufix " street"
-    private static final String STREET_PREFIX_EN = "str. ";
-    private static final String HOUSE_PREFIX = "б. ";
-    private static final String HOUSE_PREFIX_EN = "";
-    private static final String CITY_PREFIX = "м. ";
-    private static final String CITY_PREFIX_EN = "";
-    private static final String APARTMENT_PREFIX = "кв. ";
-    private static final String APARTMENT_PREFIX_EN = "ap. ";
-    private static final String DISTRICT_SUFIX = " р-н.";
-    private static final String DISTRICT_SUFIX_EN = "";
+	private static final String DELIMITER = ", ";
+	private static final String REGION_SUFIX = " oбл.";
+	private static final String REGION_SUFIX_EN = " region";
+	private static final String STREET_PREFIX = "вул. ";
+	private static final String STREET_PREFIX_EN = "str. ";
+	private static final String HOUSE_PREFIX = "б. ";
+	private static final String HOUSE_PREFIX_EN = "";
+	private static final String CITY_PREFIX = "м. ";
+	private static final String CITY_PREFIX_EN = "";
+	private static final String APARTMENT_PREFIX = "кв. ";
+	private static final String APARTMENT_PREFIX_EN = "ap. ";
+	private static final String DISTRICT_SUFIX = " р-н.";
+	private static final String DISTRICT_SUFIX_EN = " distr.";
 
-    @Id
-    @GeneratedValue
-    private Integer addressId;
+	@Id
+	@GeneratedValue
+	private Integer addressId;
 
-    @OneToOne
-    @JoinColumn(name = "userId", unique = true, nullable = false, updatable = false, insertable = true)
-    private User user;
+	@OneToOne
+	@JoinColumn(name = "userId", unique = true, nullable = false, updatable = false, insertable = true)
+	private User user;
 
-    private Integer postalCode;
+	private Integer postalCode;
 
-    private String region;
+	private String region;
 
-    private String regionEn;
+	private String regionEn;
 
-    private String district;
+	private String district;
 
-    private String districtEn;
+	private String districtEn;
 
-    private String city;
+	private String city;
 
-    private String cityEn;
+	private String cityEn;
 
-    private String street;
+	private String street;
 
-    private String streetEn;
+	private String streetEn;
 
-    private Integer house;
+	private Integer house;
 
-    @Column(nullable = true, length = 1)
-    private String houseChar;
+	@Column(nullable = true, length = 1)
+	private String houseChar;
 
-    @Column(nullable = true, length = 1)
-    private String houseCharEn;
+	@Column(nullable = true, length = 1)
+	private String houseCharEn;
 
-    private Integer slashHouse;
+	private Integer slashHouse;
 
-    private Integer apartment;
+	private Integer apartment;
 
-    @Column(nullable = true, length = 1)
-    private String apartmentChar;
+	@Column(nullable = true, length = 1)
+	private String apartmentChar;
 
-    @Column(nullable = true, length = 1)
-    private String apartmentCharEn;
+	@Column(nullable = true, length = 1)
+	private String apartmentCharEn;
 
-    public String getTextUa() {
-        StringBuffer buffer = new StringBuffer();
-        buffer.append(StrUtil.isFirstCharUpperOrDigit(street) ? STREET_PREFIX + street : street);
-        if (house != null) {
-            buffer.append(DELIMITER + HOUSE_PREFIX + house);
-            buffer.append(StringUtils.hasText(houseChar) ? "-" + houseChar : "");
-            buffer.append(slashHouse != null ? "/" + slashHouse : "");
-        }
-        if (apartment != null) {
-            buffer.append(DELIMITER + APARTMENT_PREFIX + apartment);
-            buffer.append(StringUtils.hasText(apartmentChar) ? apartmentChar : "");
-        }
-        buffer.append(DELIMITER);
-        buffer.append(StrUtil.isFirstCharUpperOrDigit(city) ? CITY_PREFIX + city : city);
-        buffer.append(StringUtils.hasText(district) ? DELIMITER + district + DISTRICT_SUFIX : "");
-        buffer.append(StringUtils.hasText(region) ? DELIMITER + region + REGION_SUFIX : "");
-        return buffer.toString().trim();
-    }
+	public String getTextUa() {
+		StringBuffer buffer = new StringBuffer();
+		if (StringUtils.hasText(street)) {
+			buffer.append(StrUtil.isFirstCharUpperOrDigit(street) ? STREET_PREFIX
+					: "");
+			buffer.append(street);
+			if (house != null) {
+				buffer.append(DELIMITER + HOUSE_PREFIX + house);
+				buffer.append(StringUtils.hasText(houseChar) ? "-" + houseChar
+						: "");
+				buffer.append(slashHouse != null ? "/" + slashHouse : "");
+			}
+			if (apartment != null) {
+				buffer.append(DELIMITER + APARTMENT_PREFIX + apartment);
+				buffer.append(StringUtils.hasText(apartmentChar) ? apartmentChar
+						: "");
+			}
+			buffer.append(DELIMITER);
+		}
+		buffer.append(StrUtil.isFirstCharUpperOrDigit(city) ? CITY_PREFIX
+				+ city : city);
+		buffer.append(StringUtils.hasText(district) ? DELIMITER + district
+				+ DISTRICT_SUFIX : "");
+		buffer.append(StringUtils.hasText(region) ? DELIMITER + region
+				+ REGION_SUFIX : "");
+		return buffer.toString().trim();
+	}
 
-    public String getTextEn() {
-        StringBuffer buffer = new StringBuffer();
-        buffer.append(StrUtil.isFirstCharUpperOrDigit(streetEn) ? STREET_PREFIX_EN + streetEn : streetEn);
-        if (house != null) {
-            buffer.append(DELIMITER + HOUSE_PREFIX_EN + house);
-            buffer.append(StringUtils.hasText(houseCharEn) ? "-" + houseCharEn : "");
-            buffer.append(slashHouse != null ? "/" + slashHouse : "");
-        }
-        if (apartment != null) {
-            buffer.append(DELIMITER + APARTMENT_PREFIX_EN + apartment);
-            buffer.append(StringUtils.hasText(apartmentCharEn) ? apartmentCharEn : "");
-        }
-        buffer.append(DELIMITER);
-        buffer.append(StrUtil.isFirstCharUpperOrDigit(city) ? CITY_PREFIX_EN + cityEn : cityEn);
-        buffer.append(StringUtils.hasText(districtEn) ? DELIMITER + districtEn + DISTRICT_SUFIX_EN : "");
-        buffer.append(StringUtils.hasText(regionEn) ? DELIMITER + regionEn + REGION_SUFIX_EN : "");
-        return buffer.toString().trim();
-    }
+	public String getTextEn() {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(StrUtil.isFirstCharUpperOrDigit(streetEn) ? STREET_PREFIX_EN
+				+ streetEn
+				: streetEn);
+		if (house != null) {
+			buffer.append(DELIMITER + HOUSE_PREFIX_EN + house);
+			buffer.append(StringUtils.hasText(houseCharEn) ? "-" + houseCharEn
+					: "");
+			buffer.append(slashHouse != null ? "/" + slashHouse : "");
+		}
+		if (apartment != null) {
+			buffer.append(DELIMITER + APARTMENT_PREFIX_EN + apartment);
+			buffer.append(StringUtils.hasText(apartmentCharEn) ? apartmentCharEn
+					: "");
+		}
+		buffer.append(DELIMITER);
+		buffer.append(StrUtil.isFirstCharUpperOrDigit(city) ? CITY_PREFIX_EN
+				+ cityEn : cityEn);
+		buffer.append(StringUtils.hasText(districtEn) ? DELIMITER + districtEn
+				+ DISTRICT_SUFIX_EN : "");
+		buffer.append(StringUtils.hasText(regionEn) ? DELIMITER + regionEn
+				+ REGION_SUFIX_EN : "");
+		return buffer.toString().trim();
+	}
 
-    public Integer getAddressId() {
-        return addressId;
-    }
+	public Integer getAddressId() {
+		return addressId;
+	}
 
-    public void setAddressId(Integer addressId) {
-        this.addressId = addressId;
-    }
+	public void setAddressId(Integer addressId) {
+		this.addressId = addressId;
+	}
 
-    public User getUser() {
-        return user;
-    }
+	public User getUser() {
+		return user;
+	}
 
-    public void setUser(User user) {
-        this.user = user;
-    }
+	public void setUser(User user) {
+		this.user = user;
+	}
 
-    public Integer getPostalCode() {
-        return postalCode;
-    }
+	public Integer getPostalCode() {
+		return postalCode;
+	}
 
-    public void setPostalCode(Integer postalCode) {
-        this.postalCode = postalCode;
-    }
+	public void setPostalCode(Integer postalCode) {
+		this.postalCode = postalCode;
+	}
 
-    public String getRegion() {
-        return region;
-    }
+	public String getRegion() {
+		return region;
+	}
 
-    public void setRegion(String region) {
-        this.region = region;
-    }
+	public void setRegion(String region) {
+		this.region = region;
+	}
 
-    public String getRegionEn() {
-        return regionEn;
-    }
+	public String getRegionEn() {
+		return regionEn;
+	}
 
-    public void setRegionEn(String regionEn) {
-        this.regionEn = regionEn;
-    }
+	public void setRegionEn(String regionEn) {
+		this.regionEn = regionEn;
+	}
 
-    public String getDistrict() {
-        return district;
-    }
+	public String getDistrict() {
+		return district;
+	}
 
-    public void setDistrict(String district) {
-        this.district = district;
-    }
+	public void setDistrict(String district) {
+		this.district = district;
+	}
 
-    public String getDistrictEn() {
-        return districtEn;
-    }
+	public String getDistrictEn() {
+		return districtEn;
+	}
 
-    public void setDistrictEn(String districtEn) {
-        this.districtEn = districtEn;
-    }
+	public void setDistrictEn(String districtEn) {
+		this.districtEn = districtEn;
+	}
 
-    public String getCity() {
-        return city;
-    }
+	public String getCity() {
+		return city;
+	}
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+	public void setCity(String city) {
+		this.city = city;
+	}
 
-    public String getCityEn() {
-        return cityEn;
-    }
+	public String getCityEn() {
+		return cityEn;
+	}
 
-    public void setCityEn(String cityEn) {
-        this.cityEn = cityEn;
-    }
+	public void setCityEn(String cityEn) {
+		this.cityEn = cityEn;
+	}
 
-    public String getStreet() {
-        return street;
-    }
+	public String getStreet() {
+		return street;
+	}
 
-    public void setStreet(String street) {
-        this.street = street;
-    }
+	public void setStreet(String street) {
+		this.street = street;
+	}
 
-    public String getStreetEn() {
-        return streetEn;
-    }
+	public String getStreetEn() {
+		return streetEn;
+	}
 
-    public void setStreetEn(String streetEn) {
-        this.streetEn = streetEn;
-    }
+	public void setStreetEn(String streetEn) {
+		this.streetEn = streetEn;
+	}
 
-    public Integer getHouse() {
-        return house;
-    }
+	public Integer getHouse() {
+		return house;
+	}
 
-    public void setHouse(Integer house) {
-        this.house = house;
-    }
+	public void setHouse(Integer house) {
+		this.house = house;
+	}
 
-    public Integer getSlashHouse() {
-        return slashHouse;
-    }
+	public Integer getSlashHouse() {
+		return slashHouse;
+	}
 
-    public void setSlashHouse(Integer slashHouse) {
-        this.slashHouse = slashHouse;
-    }
+	public void setSlashHouse(Integer slashHouse) {
+		this.slashHouse = slashHouse;
+	}
 
-    public Integer getApartment() {
-        return apartment;
-    }
+	public Integer getApartment() {
+		return apartment;
+	}
 
-    public void setApartment(Integer apartment) {
-        this.apartment = apartment;
-    }
+	public void setApartment(Integer apartment) {
+		this.apartment = apartment;
+	}
 
-    public String getHouseChar() {
-        return houseChar;
-    }
+	public String getHouseChar() {
+		return houseChar;
+	}
 
-    public void setHouseChar(String houseChar) {
-        this.houseChar = houseChar;
-    }
+	public void setHouseChar(String houseChar) {
+		this.houseChar = houseChar;
+	}
 
-    public String getHouseCharEn() {
-        return houseCharEn;
-    }
+	public String getHouseCharEn() {
+		return houseCharEn;
+	}
 
-    public void setHouseCharEn(String houseCharEn) {
-        this.houseCharEn = houseCharEn;
-    }
+	public void setHouseCharEn(String houseCharEn) {
+		this.houseCharEn = houseCharEn;
+	}
 
-    public String getApartmentChar() {
-        return apartmentChar;
-    }
+	public String getApartmentChar() {
+		return apartmentChar;
+	}
 
-    public void setApartmentChar(String apartmentChar) {
-        this.apartmentChar = apartmentChar;
-    }
+	public void setApartmentChar(String apartmentChar) {
+		this.apartmentChar = apartmentChar;
+	}
 
-    public String getApartmentCharEn() {
-        return apartmentCharEn;
-    }
+	public String getApartmentCharEn() {
+		return apartmentCharEn;
+	}
 
-    public void setApartmentCharEn(String apartmentCharEn) {
-        this.apartmentCharEn = apartmentCharEn;
-    }
+	public void setApartmentCharEn(String apartmentCharEn) {
+		this.apartmentCharEn = apartmentCharEn;
+	}
 
 }
