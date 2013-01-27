7a5fa0826c4a0b18566085a1b26aa3df010d8cf6
tumakha <tumakha@gmail.com>
2013-01-27 20:47:49 +0000

    Users ListGrid

--- src/main/java/ua/org/tumakha/spdtool/smartgwt/client/SpdSmart.java
+++ src/main/java/ua/org/tumakha/spdtool/smartgwt/client/SpdSmart.java
@@ -1,7 +1,14 @@
 package ua.org.tumakha.spdtool.smartgwt.client;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.grid.ListGrid;
 import com.smartgwt.client.widgets.grid.ListGridField;
@@ -9,50 +16,58 @@ import com.smartgwt.client.widgets.grid.ListGridRecord;
 
 public class SpdSmart implements EntryPoint {
 
-	// @Autowired
-	// private UserService userService =
-	// WebApplicationContextUtils.getWebApplicationContext(getServletContext());;
+	private final UserServiceAsync userService = GWT.create(UserService.class);
 
 	@Override
 	public void onModuleLoad() {
 
 		Canvas canvas = new Canvas();
 
-		final ListGrid countryGrid = new ListGrid();
-		countryGrid.setWidth(500);
-		countryGrid.setHeight(224);
-		countryGrid.setShowAllRecords(true);
+		final ListGrid usersGrid = new ListGrid();
+		usersGrid.setWidth(1000);
+		usersGrid.setHeight(700);
+		usersGrid.setShowAllRecords(true);
 
-		ListGridField userIdField = new ListGridField("userId", "User ID");
-		ListGridField pinField = new ListGridField("pin", "PIN");
-		ListGridField lastnameField = new ListGridField("lastname", "Lastname");
-		ListGridField firstnameField = new ListGridField("firstname",
+		ListGridField userIdField = new ListGridField("userId", "ID");
+		ListGridField pinField = new ListGridField("pin", "ІПН");
+		ListGridField lastnameField = new ListGridField("lastname", "Прізвище");
+		ListGridField firstnameField = new ListGridField("firstname", "Ім'я");
+		ListGridField lastnameEnField = new ListGridField("lastnameEn",
+				"Lastname");
+		ListGridField firstnameEnField = new ListGridField("firstnameEn",
 				"Firstname");
-		countryGrid.setFields(userIdField, pinField, lastnameField,
-				firstnameField);
-		countryGrid.setCanResizeFields(true);
-		countryGrid.setData(getUserRecords());
-		canvas.addChild(countryGrid);
+		ListGridField activeField = new ListGridField("active", "Активний");
+		usersGrid.setFields(userIdField, pinField, lastnameField,
+				firstnameField, firstnameEnField, lastnameEnField, activeField);
+		usersGrid.setCanResizeFields(true);
+		canvas.addChild(usersGrid);
+
+		userService.findAll(new AsyncCallback<List<Map<String, String>>>() {
+			@Override
+			public void onFailure(final Throwable caught) {
+				SC.warn(caught.getClass().getName() + ": "
+						+ caught.getMessage());
+			}
+
+			@Override
+			public void onSuccess(List<Map<String, String>> result) {
+				List<ListGridRecord> records = new ArrayList<ListGridRecord>(
+						result.size());
+				for (Map<String, String> properties : result) {
+					ListGridRecord record = new ListGridRecord();
+					for (String property : properties.keySet()) {
+						record.setAttribute(property, properties.get(property));
+					}
+					records.add(record);
+				}
+				ListGridRecord[] recordsArray = new ListGridRecord[result
+						.size()];
+				records.toArray(recordsArray);
+				usersGrid.setData(recordsArray);
+			}
+		});
 
 		RootPanel.get("usersListContainer").add(canvas);
-	}
-
-	private ListGridRecord[] getUserRecords() {
-		// List<User> users = userService.findAllUsers();
-		// List<ListGridRecord> records = new ArrayList<ListGridRecord>(
-		// users.size());
-		// for (User user : users) {
-		// ListGridRecord record = new ListGridRecord();
-		// record.setAttribute("userId", user.getUserId());
-		// record.setAttribute("pin", user.getPin());
-		// record.setAttribute("lastname", user.getLastname());
-		// record.setAttribute("firstname", user.getFirstname());
-		// records.add(record);
-		// }
-		// ListGridRecord[] recordsArray = new ListGridRecord[users.size()];
-		// records.toArray(recordsArray);
-		// return recordsArray;
-		return new ListGridRecord[] {};
 	}
 
 }
--- src/main/java/ua/org/tumakha/spdtool/smartgwt/client/UserService.java
+++ src/main/java/ua/org/tumakha/spdtool/smartgwt/client/UserService.java
@@ -1,12 +1,17 @@
 package ua.org.tumakha.spdtool.smartgwt.client;
 
+import java.util.List;
+import java.util.Map;
+
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 /**
  * The client side stub for the RPC service.
  */
-@RemoteServiceRelativePath("greet")
-public interface GreetingService extends RemoteService {
-	String greetServer(String name) throws IllegalArgumentException;
+@RemoteServiceRelativePath("user")
+public interface UserService extends RemoteService {
+
+	List<Map<String, String>> findAll();
+
 }
