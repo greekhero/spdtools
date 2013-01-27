955838e8d96ea73a7876ce7dc516adf514c5b495
tumakha@gmail.com <tumakha@gmail.com@836365bd-fcbe-2b9d-eda4-b5ef0fddd20a>
2013-01-27 20:47:49 +0000

    Users ListGrid

--- trunk/src/main/java/ua/org/tumakha/spdtool/smartgwt/client/UserServiceAsync.java
+++ trunk/src/main/java/ua/org/tumakha/spdtool/smartgwt/client/UserServiceAsync.java
@@ -1,28 +1,29 @@
 package ua.org.tumakha.spdtool.smartgwt.client;
 
+import java.util.List;
+import java.util.Map;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
-public interface GreetingServiceAsync {
+public interface UserServiceAsync {
 
 	/**
 	 * GWT-RPC service asynchronous (client-side) interface
 	 * 
-	 * @see ua.org.tumakha.spdtool.client.GreetingService
+	 * @see ua.org.tumakha.spdtool.UserService.GreetingService
 	 */
-	void greetServer(java.lang.String name,
-			AsyncCallback<java.lang.String> callback);
+	void findAll(AsyncCallback<List<Map<String, String>>> callback);
 
 	/**
 	 * Utility class to get the RPC Async interface from client-side code
 	 */
 	public static final class Util {
-		private static GreetingServiceAsync instance;
+		private static UserServiceAsync instance;
 
-		public static final GreetingServiceAsync getInstance() {
+		public static final UserServiceAsync getInstance() {
 			if (instance == null) {
-				instance = (GreetingServiceAsync) GWT
-						.create(GreetingService.class);
+				instance = (UserServiceAsync) GWT.create(UserService.class);
 			}
 			return instance;
 		}
