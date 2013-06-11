469bed3331fb6ee9210d1267023f8e1268a0b18a
tumakha <tumakha@gmail.com>
2013-06-11 20:18:48 +0000

    RentType.EQUIPMENT

--- src/main/java/ua/org/tumakha/spdtool/enums/RentType.java
+++ src/main/java/ua/org/tumakha/spdtool/enums/RentType.java
@@ -5,5 +5,26 @@ package ua.org.tumakha.spdtool.enums;
  */
 public enum RentType {
 
-	NONE, OFFICE, OFFICE_EQUIPMENT;
+	NONE(0, ""),
+	OFFICE(642, "офісного приміщення"),
+	OFFICE_EQUIPMENT(1350, "офісного приміщення та обладнання"),
+	EQUIPMENT(708, "обладнання");
+
+	private int amount;
+
+	private String description;
+
+	private RentType(int amount, String description) {
+		this.amount = amount;
+		this.description = description;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
 }
