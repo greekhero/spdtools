48e98f1baeff36dfa372520890762667fd2fa002
tumakha <tumakha@gmail.com>
2013-09-27 19:19:16 +0000

    BankDataBuilder

---
+++ src/main/java/ua/org/tumakha/spdtool/template/xls/row/BankDataBuilder.java
@@ -0,0 +1,117 @@
+package ua.org.tumakha.spdtool.template.xls.row;
+
+import ua.org.tumakha.spdtool.entity.BankTransaction;
+import ua.org.tumakha.spdtool.template.model.BankDay;
+import ua.org.tumakha.spdtool.template.model.BankOperationRow;
+import ua.org.tumakha.spdtool.template.model.BankQuarter;
+import ua.org.tumakha.util.PaymentPurposeUtil;
+
+import java.util.*;
+
+/**
+ * @author Yuriy Tumakha
+ */
+public class BankDataBuilder {
+
+    private Map<Integer, Object> metaDataMap = new LinkedHashMap<Integer, Object>();
+    private Map<String, Integer> sellsUSD = new HashMap<String, Integer>();
+    private Map<String, Integer> incomeFromUSD = new HashMap<String, Integer>();
+
+    public void prepareTemplateModel(Map<String, Object> beans, List<BankTransaction> transactions) {
+        Map<Integer, BankDay> bankDataDays = getBankDataDays(transactions);
+        List<BankQuarter> bankData = new ArrayList<BankQuarter>();
+        BankQuarter lastQuarter = null;
+        Calendar calendar = Calendar.getInstance();
+
+        int rowNum = 9;
+        for (BankDay bankDay : bankDataDays.values()) {
+            calendar.setTime(bankDay.getDate());
+            int month = calendar.get(Calendar.MONTH) + 1;
+            int lastQuarterNumber = lastQuarter == null ? (int) Math.ceil((double) month / 3) - 1 : lastQuarter.getNumber();
+            if (month > lastQuarterNumber * 3) {
+                lastQuarter = new BankQuarter(lastQuarterNumber + 1);
+                bankData.add(lastQuarter);
+                if (bankData.size() > 1) {
+                    rowNum += lastQuarter.getNumber() > 2 ? 8 : 7;
+                }
+            }
+            for (BankOperationRow bankTransaction : bankDay.getTransactions()) {
+                lastQuarter.addTransaction(bankTransaction);
+                saveMetaData(rowNum, bankTransaction);
+                rowNum++;
+            }
+        }
+
+        beans.put("bankData", bankData);
+        beans.put("metaData", metaDataMap);
+    }
+
+    private Integer getUSDTransitAccount(List<BankTransaction> transactions) {
+        int i = 0;
+        for (BankTransaction transaction : transactions) {
+            String purpose = transaction.getPlatPurpose();
+            if (purpose != null && purpose.startsWith("@")) {
+                return transaction.getAccountId();
+            }
+            if (i++ > 20) {
+                return null;
+            }
+        }
+        return null;
+    }
+
+    private Map<Integer, BankDay> getBankDataDays(List<BankTransaction> transactions) {
+        Integer usdTransitAccount = getUSDTransitAccount(transactions);
+        Map<Integer, BankDay> bankDataDays = new LinkedHashMap<Integer, BankDay>();
+        for (BankTransaction transaction : transactions) {
+            String purpose = transaction.getPlatPurpose();
+            if (transaction.getAccountId().equals(usdTransitAccount)
+                    || (purpose != null && purpose.length() < 20 && purpose.startsWith("RBU"))) {
+                continue; // ignore transaction
+            }
+
+            Integer transactionDate = transaction.getArcDate();
+            if (bankDataDays.get(transactionDate) == null) {
+                bankDataDays.put(transactionDate, new BankDay(transactionDate, transaction));
+            } else {
+                bankDataDays.get(transactionDate).addTransaction(transaction);
+            }
+        }
+        return bankDataDays;
+    }
+
+    private void saveMetaData(int rowNum, BankOperationRow bankTransaction) {
+        if (bankTransaction.isIncomeUAH() || bankTransaction.isAccountUSD()) {
+            OperationRowMetaData rowMetaData = new OperationRowMetaData();
+            rowMetaData.setIncomeUSD(bankTransaction.isIncomeUSD());
+            rowMetaData.setExpenseUSD(bankTransaction.isExpenseUSD());
+            rowMetaData.setIncomeUAH(bankTransaction.isIncomeUAH());
+            rowMetaData.setCommission(bankTransaction.getCommission() instanceof Double);
+            if (bankTransaction.isExpenseUSD()) {
+                String dateAmountKey = PaymentPurposeUtil.getDateAmountKey(bankTransaction.getDate(), bankTransaction.getSellAmountUSD());
+                if (incomeFromUSD.get(dateAmountKey) != null) {
+                    Integer row = incomeFromUSD.get(dateAmountKey);
+                    OperationRowMetaData rowData = (OperationRowMetaData) metaDataMap.get(row);
+                    if (rowData != null) {
+                        rowData.setUsdRowNum(rowNum);
+                    }
+                    incomeFromUSD.remove(dateAmountKey);
+                } else {
+                    sellsUSD.put(dateAmountKey, rowNum);
+                }
+            }
+            if (rowMetaData.isCommission()) {
+                String dateAndAmountUSD = PaymentPurposeUtil.getIncomeDateAndAmountUSD(bankTransaction.getOriginalPurpose());
+                Integer usdRowNum = sellsUSD.get(dateAndAmountUSD);
+                if (usdRowNum != null) {
+                    rowMetaData.setUsdRowNum(usdRowNum);
+                    sellsUSD.remove(dateAndAmountUSD);
+                } else {
+                    incomeFromUSD.put(dateAndAmountUSD, rowNum);
+                }
+            }
+            metaDataMap.put(rowNum, rowMetaData);
+        }
+    }
+
+}
