bd97c55fc30ee77aac3f8c45f5dce44962aa8cee
tumakha <tumakha@gmail.com>
2013-11-09 08:36:24 +0000

    generationTime

--- src/main/java/ua/org/tumakha/spdtool/web/DeclarationController.java
+++ src/main/java/ua/org/tumakha/spdtool/web/DeclarationController.java
@@ -1,31 +1,13 @@
 package ua.org.tumakha.spdtool.web;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import javax.validation.Valid;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import ua.org.tumakha.spdtool.entity.Declaration;
 import ua.org.tumakha.spdtool.entity.Group;
 import ua.org.tumakha.spdtool.entity.User;
@@ -35,6 +17,9 @@ import ua.org.tumakha.spdtool.services.GroupService;
 import ua.org.tumakha.spdtool.services.TemplateService;
 import ua.org.tumakha.spdtool.services.UserService;
 import ua.org.tumakha.spdtool.web.model.DeclarationModel;
+
+import javax.validation.Valid;
+import java.util.*;
 
 /**
  * @author Yuriy Tumakha
@@ -227,10 +212,12 @@ public class DeclarationController {
 	public String generateDocuments(@Valid DeclarationModel declarationModel, RedirectAttributes redirectAttrs)
 			throws Exception {
 
+        long time = System.currentTimeMillis();
 		List<String> fileNames = templateService.generateDeclarations(declarationModel.getEnabledUserIds(),
 				declarationModel.getGroupIds(), declarationModel.getYear(), declarationModel.getQuarter());
 
 		redirectAttrs.addFlashAttribute("fileNames", fileNames);
+        redirectAttrs.addFlashAttribute("generationTime", (System.currentTimeMillis() - time) / 1000 );
 
 		return redirect("downloadDocuments");
 	}
--- src/main/java/ua/org/tumakha/spdtool/web/PaymentsController.java
+++ src/main/java/ua/org/tumakha/spdtool/web/PaymentsController.java
@@ -1,33 +1,22 @@
 package ua.org.tumakha.spdtool.web;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import javax.validation.Valid;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import ua.org.tumakha.spdtool.entity.Group;
 import ua.org.tumakha.spdtool.entity.User;
 import ua.org.tumakha.spdtool.services.GroupService;
 import ua.org.tumakha.spdtool.services.TemplateService;
 import ua.org.tumakha.spdtool.services.UserService;
 import ua.org.tumakha.spdtool.web.model.PaymentModel;
+
+import javax.validation.Valid;
+import java.util.*;
 
 /**
  * @author Yuriy Tumakha
@@ -116,10 +105,12 @@ public class PaymentsController {
 		}
 
 		log.debug("EnabledUserIds: " + paymentModel.getEnabledUserIds());
+        long time = System.currentTimeMillis();
 		List<String> fileNames = templateService.generatePaymentDocuments(paymentModel.getEnabledUserIds(),
 				paymentModel.isSendEmail());
 
 		redirectAttrs.addFlashAttribute("fileNames", fileNames);
+        redirectAttrs.addFlashAttribute("generationTime", (System.currentTimeMillis() - time) / 1000 );
 
 		return redirect("generatedDocuments");
 	}
