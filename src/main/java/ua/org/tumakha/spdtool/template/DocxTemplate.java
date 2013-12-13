168ab2d159c65045cea8b2aeddf1346bbe6e1f3f
tumakha <tumakha@gmail.com>
2013-12-13 20:41:11 +0000

    Removed second parameter

--- src/main/java/ua/org/tumakha/spdtool/template/DocxTemplate.java
+++ src/main/java/ua/org/tumakha/spdtool/template/DocxTemplate.java
@@ -5,31 +5,25 @@ package ua.org.tumakha.spdtool.template;
  */
 public enum DocxTemplate {
 	
-	ACT("Act_PE.docx", true),
-	CONTRACT_ANNEX("Contract_PE_Annex.docx", true),
-	CONTRACT("Contract_PE.docx", true),
-	CONTRACT_ADITIONAL_AGREEMENT("Contract_PE_Additional_Agreement.docx", true),
-	TAX_SYSTEM_STATEMENT("Tax_System_Statement.docx", false),
-	INCOME_CALCULATION("Income_Calculation.docx", false),
-	FORM_20_OPP("20-OPP.docx", false),
-	FORM_11_KVED("Form11_Kved.docx", true),
-	ECP_REGISTRATION("ECP_Registration.docx", true),
-	ECP_JOIN("ECP_Join.docx", true);
+	ACT("Act_PE.docx"),
+	CONTRACT_ANNEX("Contract_PE_Annex.docx"),
+	CONTRACT("Contract_PE.docx"),
+	CONTRACT_ADITIONAL_AGREEMENT("Contract_PE_Additional_Agreement.docx"),
+	TAX_SYSTEM_STATEMENT("Tax_System_Statement.docx"),
+	INCOME_CALCULATION("Income_Calculation.docx"),
+	FORM_20_OPP("20-OPP.docx"),
+	FORM_11_KVED("Form11_Kved.docx"),
+	ECP_REGISTRATION("ECP_Registration.docx"),
+	ECP_JOIN("ECP_Join.docx");
 
 	private String filename;
-	private boolean freemarker;
 
-	private DocxTemplate(String filename, boolean freemarker) {
+	private DocxTemplate(String filename) {
 		this.filename = filename;
-		this.freemarker = freemarker;
 	}
 
 	public String getFilename() {
 		return filename;
-	}
-
-	public boolean isFreemarker() {
-		return freemarker;
 	}
 
 }
